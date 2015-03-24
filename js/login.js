6b5d1cd5354b6290f7564409b1c76a8d4ebff7f2
charle692 <charle692@gmail.com>
2015-03-23 23:02:06 -0400

    created login page and added bootrstrap and JQuery

---
+++ js/login.js
@@ -0,0 +1,27 @@
+particlesJS('particles-js', {
+  particles: {
+    color: '#0D47A1',
+    shape: 'circle',
+    opacity: 1,
+    size: 3,
+    size_random: true,
+    nb: 50,
+    line_linked: {
+      enable_auto: false,
+      distance: 250,
+      color: '#fff',
+      opacity: 0.5,
+      width: 1,
+      condensed_mode: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 600
+      }
+    },
+    anim: {
+      enable: true,
+      speed: 2
+    }
+  },
+  retina_detect: true
+});

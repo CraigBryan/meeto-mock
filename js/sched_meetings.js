bf7430c90e5654ef074b6aefac26d069fbd47428
Craig <craigbryan.csb@gmail.com>
2015-03-28 22:18:16 -0400

    Mostly finished the scheduled meetings page.

---
+++ js/sched_meetings.js
@@ -0,0 +1,15 @@
+$("#past_modal_button").on('click', function() {
+  $("#past_modal").modal();
+});
+
+$("#future_modal_1_button").on('click', function() {
+  $("#future_modal_1").modal();
+});
+
+$("#future_modal_2_button").on('click', function() {
+  $("#future_modal_2").modal();
+});
+
+$("#future_modal_3_button").on('click', function() {
+  $("#future_modal_3").modal();
+});
